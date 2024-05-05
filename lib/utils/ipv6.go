1c3729fc4ad645e75b9423df09ed7a85f82c38a8
Antonio Cheong <acheong@duti.dev>
2024-05-05 05:18:38 +0100

    feat: Encode/Decode arbitrary sized byte arrays

--- lib/utils/ipv6.go
+++ lib/utils/ipv6.go
@@ -1,46 +1,98 @@
 package utils
 
 import (
+	"crypto/rand"
 	"encoding/binary"
-	"errors"
+	"math"
 	"net"
 )
 
-const ErrTooMuchData = "unable to encode this much data"
+func EncodeIPv6(b []byte, r [16]byte) (ips []net.IP, ports []uint16) {
+	size := int(math.Ceil(float64(len(b)+4) / 16))
+	// Pre-allocate the ips slice (len(b) + 4) / 16
+	ips = make([]net.IP, size)
+	ports = make([]uint16, size)
+	var chunk [16]byte
+	// Take the first 4 bytes encode the length of the full data
+	binary.BigEndian.PutUint32(chunk[:], uint32(len(b)))
 
-func BytesToAddress(b []byte, r [16]byte) (ip net.IP, port uint16, err error) {
-	ip, err = bytesToIPv6(b)
-	if err != nil {
-		return
+	// Then take 12 bytes from the data to fill in the rest of the chunk
+	copy(chunk[4:], b[:12])
+
+	ips[0], ports[0] = ChunkToAddress(chunk[:], r)
+	// Then for each 16 byte chunk, encode it into an address
+	n := 12
+	for i := 1; i < size; i++ {
+		var chunk [16]byte
+		if n+16 < len(b) {
+			copy(chunk[:], b[n:n+16])
+			n += 16
+		} else {
+			copy(chunk[:], b[n:])
+			// Fill in the rest with random bytes
+			rand.Read(chunk[len(b)-n:])
+		}
+		ips[i], ports[i] = ChunkToAddress(chunk[:], r)
 	}
+	return
+}
+
+func DecodeIPv6(ips []net.IP, ports []uint16, r [16]byte) []byte {
+	// Get the length of the full data by decoding the first chunk
+	chunk := ChunkToBytes(ips[0], ports[0], r)
+	length := binary.BigEndian.Uint32(chunk[:4])
+	// Pre-allocate the data slice
+	data := make([]byte, length)
+	// Copy the first 12 bytes from the first chunk
+	copy(data[:], chunk[4:])
+	n := 12
+	// Then for each chunk, decode it into the data slice
+	for i := 1; i < len(ips); i++ {
+		chunk = ChunkToBytes(ips[i], ports[i], r)
+		if n+16 < len(data) {
+			copy(data[n:], chunk[:])
+		} else {
+			copy(data[n:], chunk[:len(data)-n])
+		}
+		n += 16
+	}
+	return data
+}
+
+func ChunkToAddress(b []byte, r [16]byte) (ip net.IP, port uint16) {
+	ip = bytesToIPv6(b)
+	// Run it through xor at least once to ensure randomness
+	ip = xorIpv6(ip, r)
 	ip, port = toSafeAddress(ip, r)
 	return
 }
 
-func AddressToBytes(ip net.IP, port uint16, rand [16]byte) []byte {
-	port--
+func ChunkToBytes(ip net.IP, port uint16, rand [16]byte) []byte {
+	// Copy the IP to avoid modifying the original slice
+	ip = append([]byte{}, ip...)
 	for i := 0; i < len(rand); i += 2 {
 		port ^= binary.BigEndian.Uint16(rand[i:])
 	}
-	for i := uint16(0); i < port-1; i++ {
+	for i := uint16(0); i < port; i++ {
 		ip = xorIpv6(ip, rand)
 	}
 	return ip
 }
 
-func bytesToIPv6(b []byte) (net.IP, error) {
-	if len(b) > 16 {
-		return nil, errors.New(ErrTooMuchData)
+func bytesToIPv6(b []byte) net.IP {
+	if len(b) != 16 {
+		panic("invalid length")
 	}
-	if len(b) < 16 {
-		b = append(make([]byte, 16-len(b)), b...)
-	}
-	return net.IP(b), nil
+	// Copy the bytes to a new slice to avoid the slice pointing to the original slice
+	return net.IP(append([]byte{}, b...))
 }
 
 func xorIpv6(ip net.IP, rand [16]byte) net.IP {
+	// We do this inefficient mess in case any significant random bytes are 0
 	for i := range ip {
-		ip[i] ^= rand[i]
+		for j := range rand {
+			ip[i] ^= rand[j]
+		}
 	}
 	return ip
 }
@@ -51,6 +103,9 @@ func toSafeAddress(ip net.IP, rand [16]byte) (newIp net.IP, port uint16) {
 		if !newIp.IsGlobalUnicast() {
 			newIp = xorIpv6(ip, rand)
 			port++
+			if port > 100 {
+				panic("unable to find a address, did you forget to initialize random?")
+			}
 		} else {
 			break
 		}
--- lib/utils/ipv6_test.go
+++ lib/utils/ipv6_test.go
@@ -6,25 +6,67 @@ import (
 	"testing"
 )
 
+func TestChunks(t *testing.T) {
+	testCases := []struct {
+		name  string
+		bytes [16]byte
+	}{
+		{
+			name: "random bytes",
+		},
+		{
+			name:  "all zeros",
+			bytes: [16]byte{},
+		},
+		{
+			name:  "all 0xFFs",
+			bytes: [16]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Logf("Test case %s\n", tc.name)
+		t.Run(tc.name, func(t *testing.T) {
+			var r [16]byte
+			if n, err := rand.Read(r[:]); err != nil || n != 16 {
+				t.Fatal("unable to generate random bytes")
+			}
+
+			if tc.name == "random bytes" {
+				rand.Read(tc.bytes[:])
+			}
+
+			ip, port := utils.ChunkToAddress(tc.bytes[:], r)
+			restored := utils.ChunkToBytes(ip, port, r)
+			for i := range restored {
+				if restored[i] != tc.bytes[i] {
+					t.Fatalf("restored byte %d is different", i)
+				}
+			}
+			t.Logf("IP %v, port %d", ip, port)
+		})
+	}
+}
+
 func TestByteToIPv6(t *testing.T) {
-	var b [16]byte
+	var b [238]byte
 	var r [16]byte
-	if n, err := rand.Read(b[:]); err != nil || n != 16 {
+	if _, err := rand.Read(b[:]); err != nil {
 		t.Fatal("unable to generate random bytes")
 	}
 	if n, err := rand.Read(r[:]); err != nil || n != 16 {
 		t.Fatal("unable to generate random bytes")
 	}
 
-	ip, port, err := utils.BytesToAddress(b[:], r)
-	if err != nil {
-		t.Fatal(err)
+	ips, ports := utils.EncodeIPv6(b[:], r)
+	data := utils.DecodeIPv6(ips, ports, r)
+
+	if len(b) != len(data) {
+		t.Fatal("data length mismatch")
 	}
-	restored := utils.AddressToBytes(ip, port, r)
-	for i := range restored {
-		if restored[i] != b[i] {
-			t.Fatalf("restored byte %d is different", i)
+	for i := 0; i < len(b); i++ {
+		if b[i] != data[i] {
+			t.Fatalf("data mismatch at index %d", i)
 		}
 	}
-	t.Logf("IP %v, port %d", ip, port)
 }
