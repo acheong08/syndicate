8329ee408e1ce2830bcb732307e01a57d3c4c869
Antonio Cheong <acheong@duti.dev>
2025-05-12 01:48:16 +0100

    feat (tested): HTTP server on single relay connection

--- v2/lib/http.go
+++ v2/lib/http.go
@@ -13,41 +13,41 @@ import (
 	"github.com/syncthing/syncthing/lib/relay/protocol"
 )
 
-func ServeMux(ctx context.Context, mux *http.ServeMux, cert tls.Certificate, numRelays int) error {
-	if numRelays < 1 {
-		numRelays = 1
-	}
-	relays, err := relay.FindOptimal(ctx, "DE", numRelays)
-	if err != nil {
-		return err
-	}
-
-	if len(relays.Relays) < numRelays {
-		return fmt.Errorf("not enough relays available")
-	}
-	connChan := make(chan net.Conn, numRelays)
+func ServeMux(ctx context.Context, relayAddress string, mux *http.ServeMux, cert tls.Certificate) error {
+	// if numRelays < 1 {
+	// 	numRelays = 1
+	// }
+	// relays, err := relay.FindOptimal(ctx, "DE", numRelays)
+	// if err != nil {
+	// 	return err
+	// }
+	//
+	// if len(relays.Relays) < numRelays {
+	// 	return fmt.Errorf("not enough relays available")
+	// }
+	connChan := make(chan net.Conn)
 	listener := ConnListener{connChan, make(chan struct{})}
-	for i := range min(len(relays.Relays), numRelays) {
-		invite, err := relay.Listen(ctx, relays.Relays[i].URL, cert)
-		if err != nil {
-			return eris.Wrap(err, "could not listen on relay")
-		}
-		go func(invites <-chan protocol.SessionInvitation) {
-			for {
-				select {
-				case invite := <-invites:
-					conn, _, err := relay.CreateSession(ctx, invite, cert, nil)
-					if err != nil {
-						log.Printf("ERROR: Relay session creation failed with %s", err)
-						continue
-					}
-					connChan <- conn
-				case <-ctx.Done():
-					return
-				}
-			}
-		}(invite)
+	// for i := range min(len(relays.Relays), numRelays) {
+	invite, err := relay.Listen(ctx, relayAddress, cert)
+	if err != nil {
+		return eris.Wrap(err, "could not listen on relay")
 	}
+	go func(invites <-chan protocol.SessionInvitation) {
+		for {
+			select {
+			case invite := <-invites:
+				conn, _, err := relay.CreateSession(ctx, invite, cert, nil)
+				if err != nil {
+					log.Printf("ERROR: Relay session creation failed with %s", err)
+					continue
+				}
+				connChan <- conn
+			case <-ctx.Done():
+				return
+			}
+		}
+	}(invite)
+	// }
 	return http.Serve(listener, mux)
 }
 
---
+++ v2/lib/http_test.go
@@ -0,0 +1,81 @@
+package lib_test
+
+import (
+	"bufio"
+	"context"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/acheong08/syndicate/v2/lib"
+	"github.com/acheong08/syndicate/v2/lib/crypto"
+	"github.com/acheong08/syndicate/v2/lib/relay"
+	"github.com/syncthing/syncthing/lib/protocol"
+	relayprotocol "github.com/syncthing/syncthing/lib/relay/protocol"
+)
+
+func TestHttpServing(t *testing.T) {
+	serverCert, _ := crypto.NewCertificate("syncthing", 1)
+	serverDeviceId := protocol.NewDeviceID(serverCert.Certificate[0])
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	relays, err := relay.FindOptimal(ctx, "DE", 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+	go func() {
+		if err := lib.ServeMux(ctx, relays.First().URL, mux, serverCert); err != nil {
+			panic(err)
+		}
+	}()
+	clientCert, _ := crypto.NewCertificate("syncthing", 1)
+	invite, err := tryGetInviteUntil(ctx, relays.First().URL, serverDeviceId, clientCert, 10*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	empty := ""
+	conn, _, err := relay.CreateSession(ctx, invite, clientCert, &empty)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+	req, _ := http.NewRequest(http.MethodGet, "http://localhost/", nil)
+	if err = req.Write(conn); err != nil {
+		t.Fatal(err)
+	}
+	resp, err := http.ReadResponse(bufio.NewReader(conn), req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		t.Fatalf("Got status code %d", resp.StatusCode)
+	}
+	body, _ := io.ReadAll(resp.Body)
+	log.Println(string(body))
+}
+func tryGetInviteUntil(ctx context.Context, relayURL string, serverDeviceId protocol.DeviceID, clientCert tls.Certificate, timeout time.Duration) (relayprotocol.SessionInvitation, error) {
+	deadline := time.Now().Add(timeout)
+	var invite relayprotocol.SessionInvitation
+	var err error
+	for {
+		invite, err = relay.GetInvite(ctx, relayURL, serverDeviceId, clientCert)
+		if err == nil {
+			return invite, nil
+		}
+		if time.Now().After(deadline) {
+			return relayprotocol.SessionInvitation{}, fmt.Errorf("timeout waiting for invite: %w", err)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+}
